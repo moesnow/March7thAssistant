39c8d23da84f4643d686e742c6e581c105876335
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-02 11:47:36 +0800

    build: update Auto_Simulated_Universe script

--- scripts/Auto_Simulated_Universe.bat
+++ scripts/Auto_Simulated_Universe.bat
@@ -1,12 +1,7 @@
 @echo off
 setlocal
 
-powershell -Command "$psVersionTable.PSEdition" >nul 2>&1
-if %errorlevel% equ 0 (
-    cd .\3rdparty\Auto_Simulated_Universe
-) else (
-    cd .\3rdparty\Auto_Simulated_Universe
-)
+cd .\3rdparty\Auto_Simulated_Universe
 
 echo Installing dependencies...
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
