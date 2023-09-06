12ac80c08f9501ff335e8de527a9a2399d826371
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-06 09:54:18 +0800

    fix: update Auto_Simulated_Universe

--- scripts/update_Auto_Simulated_Universe.bat
+++ scripts/update_Auto_Simulated_Universe.bat
@@ -1,12 +1,24 @@
 @echo off
 setlocal
 
-cd .\3rdparty\Auto_Simulated_Universe
+cd .\3rdparty
 
-echo Installing dependencies...
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+chcp 65001
 
-echo Starting application...
-python update.py
+echo 下载中...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://ghproxy.com/https://github.com/CHNZYX/Auto_Simulated_Universe/archive/main.zip', '.\Auto_Simulated_Universe.zip')"
+echo 下载完成
+
+echo 开始解压...
+powershell -Command "Expand-Archive -Path '.\Auto_Simulated_Universe.zip' -DestinationPath './' -Force"
+echo 解压完成
+
+echo 开始更新...
+echo 删除文件...
+rmdir /s /q "Auto_Simulated_Universe"
+del Auto_Simulated_Universe.zip
+echo 重命名文件夹...
+ren Auto_Simulated_Universe-main Auto_Simulated_Universe
+echo 更新完成
 
 endlocal
