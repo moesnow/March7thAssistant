98524864906089ba5f5894db0ac6500182c40f83
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-08 11:33:25 +0800

    feat: exclude launcher.exe

--- tasks/base/windowswitcher.py
+++ tasks/base/windowswitcher.py
@@ -9,35 +9,35 @@ import time
 class WindowSwitcher:
     @staticmethod
     def check_and_switch(title):
-        def switch_window(title, max_retries):
-            for i in range(max_retries):
-                window = gw.getWindowsWithTitle(title)
-                if not window:
-                    continue
-                for w in window:
-                    if w.title == title:
+        return WindowSwitcher.switch_window(title, max_retries=4)
+
+    def switch_window(title, max_retries):
+        for i in range(max_retries):
+            window = gw.getWindowsWithTitle(title)
+            if not window:
+                continue
+            for w in window:
+                if w.title == title:
+                    try:
+                        hwnd = win32gui.FindWindow("UnityWndClass", title)
+                        win32gui.GetWindowRect(hwnd)
+                    except Exception as e:
+                        continue
+                    try:
+                        w.restore()
+                        w.activate()
+                    except Exception as e:
+                        logger.error(e)
+                    time.sleep(2)
+                    if w.isActive:
                         try:
-                            w.restore()
-                            w.activate()
+                            hwnd = win32gui.FindWindow("UnityWndClass", title)
+                            win32gui.GetWindowRect(hwnd)
+                            return True
                         except Exception as e:
                             logger.error(e)
-                        time.sleep(2)
-                        if w.isActive:
-                            try:
-                                hwnd = win32gui.FindWindow("UnityWndClass", title)
-                                win32gui.GetWindowRect(hwnd)
-                            except Exception as e:
-                                logger.error(e)
-                                logger.debug(_("切换窗口失败，尝试ALT+TAB"))
-                                pyautogui.hotkey('alt', 'tab')
-                                time.sleep(2)
-                                continue
-                        else:
-                            logger.debug(_("切换窗口失败，尝试ALT+TAB"))
-                            pyautogui.hotkey('alt', 'tab')
-                            time.sleep(2)
-                            continue
-
-                        return w.isActive
-            return False
-        return switch_window(title, max_retries=4)
+                    logger.info(_("切换窗口失败，尝试ALT+TAB"))
+                    pyautogui.hotkey('alt', 'tab')
+                    time.sleep(2)
+                    continue
+        return False
