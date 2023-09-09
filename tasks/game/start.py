5d900f9d22a0deffd76575332eecee8c996868ea
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-10 00:52:28 +0800

    feat: auto detect game_path

--- tasks/game/start.py
+++ tasks/game/start.py
@@ -7,6 +7,7 @@ from managers.ocr_manager import ocr
 from tasks.base.base import Base
 from tasks.game.stop import Stop
 from tasks.base.resolution import Resolution
+import psutil
 import time
 import sys
 import os
@@ -17,7 +18,7 @@ class Start:
     def check_game_path(game_path):
         if not os.path.exists(game_path):
             logger.error(_("游戏路径不存在: {path}").format(path=game_path))
-            logger.info(_("请在设置中修改游戏路径"))
+            logger.info(_("首次使用请启动游戏进入主界面后重试，程序会自动检测并保存游戏路径"))
             input(_("按任意键关闭窗口. . ."))
             sys.exit(1)
 
@@ -71,5 +72,18 @@ class Start:
                 logger.info(_("游戏启动成功"))
         else:
             logger.info(_("游戏已经启动了"))
+            Start.update_game_path(config.game_process_name)
             Resolution.check(config.game_title_name)
         return True
+
+    @staticmethod
+    def update_game_path(name):
+        for proc in psutil.process_iter(attrs=['pid', 'name']):
+            if name in proc.info['name']:
+                process = psutil.Process(proc.info['pid'])
+                program_path = process.exe()
+                if config.game_path != program_path:
+                    config.set_value("game_path", program_path)
+                    logger.info(_("游戏路径更新成功"))
+                return True
+        return False
