535386576ff860b01498161256e816ca5a2505e2
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-11 18:55:59 +0800

    fix: titleformatter error

--- module/logger/titleformatter.py
+++ module/logger/titleformatter.py
@@ -12,30 +12,33 @@ class TitleFormatter:
 
     @staticmethod
     def format_title(title, level=0):
-        separator_length = 115
-        title_lines = title.split('\n')
-        separator = '+' + '-' * separator_length + '+'
-        title_length = TitleFormatter.custom_len(title)
-        half_separator_left = (separator_length - title_length) // 2
-        half_separator_right = separator_length - title_length - half_separator_left
+        try:
+            separator_length = 115
+            title_lines = title.split('\n')
+            separator = '+' + '-' * separator_length + '+'
+            title_length = TitleFormatter.custom_len(title)
+            half_separator_left = (separator_length - title_length) // 2
+            half_separator_right = separator_length - title_length - half_separator_left
 
-        if level == 0:
-            formatted_title_lines = []
+            if level == 0:
+                formatted_title_lines = []
 
-            for line in title_lines:
-                title_length_ = TitleFormatter.custom_len(line)
-                half_separator_left_ = (separator_length - title_length_) // 2
-                half_separator_right_ = separator_length - title_length_ - half_separator_left_
+                for line in title_lines:
+                    title_length_ = TitleFormatter.custom_len(line)
+                    half_separator_left_ = (separator_length - title_length_) // 2
+                    half_separator_right_ = separator_length - title_length_ - half_separator_left_
 
-                formatted_title_line = '|' + ' ' * half_separator_left_ + line + ' ' * half_separator_right_ + '|'
-                formatted_title_lines.append(formatted_title_line)
+                    formatted_title_line = '|' + ' ' * half_separator_left_ + line + ' ' * half_separator_right_ + '|'
+                    formatted_title_lines.append(formatted_title_line)
 
-            print(separator)
-            print('\n'.join(formatted_title_lines))
-            print(separator)
-        elif level == 1:
-            formatted_title = '=' * half_separator_left + ' ' + title + ' ' + '=' * half_separator_right
-            print(f"{formatted_title}")
-        elif level == 2:
-            formatted_title = '-' * half_separator_left + ' ' + title + ' ' + '-' * half_separator_right
-            print(f"{formatted_title}")
+                print(separator)
+                print('\n'.join(formatted_title_lines))
+                print(separator)
+            elif level == 1:
+                formatted_title = '=' * half_separator_left + ' ' + title + ' ' + '=' * half_separator_right
+                print(f"{formatted_title}")
+            elif level == 2:
+                formatted_title = '-' * half_separator_left + ' ' + title + ' ' + '-' * half_separator_right
+                print(f"{formatted_title}")
+        except:
+            pass
