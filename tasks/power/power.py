6c0d634ce819b9878b0a5323389bf07029928d58
l1ttlebad <741466738@qq.com>
2023-09-16 07:08:02 +0800

    add borrow_character_from option, select character by username

--- tasks/power/power.py
+++ tasks/power/power.py
@@ -83,7 +83,50 @@ class Power:
         if not auto.find_element("支援列表", "text", max_retries=10, crop=(234 / 1920, 78 / 1080, 133 / 1920, 57 / 1080)):
             logger.error(_("未进入支援列表"))
             return False
+
         try:
+            # 尝试优先使用指定用户名的支援角色
+            if config.borrow_character_from.strip():
+                auto.mouse_click(230, 230)
+                time.sleep(0.5)
+                for i in range(5):
+                    if auto.click_element(config.borrow_character_from.strip(), "text", crop=(196 / 1920, 167 / 1080, 427 / 1920, 754 / 1080), include=True,):
+                        # 找到角色的对应处理
+                        if not auto.click_element("入队", "text", max_retries=10, crop=(1518 / 1920, 960 / 1080, 334 / 1920, 61 / 1080)):
+                            logger.error(_("找不到入队按钮"))
+                            return False
+                        # 等待界面加载
+                        time.sleep(0.5)
+                        result = auto.find_element(("解除支援", "取消"), "text", max_retries=10, include=True)
+                        if result:
+                            if auto.matched_text == "解除支援":
+                                config.daily_tasks["使用支援角色并获得战斗胜利1次"] = False
+                                config.save_config()
+                                return True
+                            elif auto.matched_text == "取消":
+                                auto.click_element_with_pos(result)
+                                auto.find_element("支援列表", "text", max_retries=10, crop=(234 / 1920, 78 / 1080, 133 / 1920, 57 / 1080))
+                                continue
+                        else:
+                            return False
+                    auto.mouse_scroll(18, -1)
+                    # 等待界面完全停止
+                    time.sleep(0.5)
+
+                logger.error(_("找不到指定用户名的支援角色，尝试按照优先级选择"))
+                # 重新打开支援页面，防止上一次的滚动位置影响
+                auto.press_key("esc")
+                time.sleep(0.5)
+                if not auto.click_element("支援", "text", max_retries=10, crop=(1670 / 1920, 700 / 1080, 225 / 1920, 74 / 1080)):
+                    logger.error(_("找不到支援按钮"))
+                    return False
+                # 等待界面加载
+                time.sleep(0.5)
+                if not auto.find_element("支援列表", "text", max_retries=10, crop=(234 / 1920, 78 / 1080, 133 / 1920, 57 / 1080)):
+                    logger.error(_("未进入支援列表"))
+                    return False
+                
+
             for name in config.borrow_character:
                 if auto.click_element("./assets/images/character/" + name + ".png", "image", 0.8, max_retries=1, scale_range=(0.8, 1.2), crop=(57 / 1920, 143 / 1080, 140 / 1920, 814 / 1080)):
                     if not auto.click_element("入队", "text", max_retries=10, crop=(1518 / 1920, 960 / 1080, 334 / 1920, 61 / 1080)):
