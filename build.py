e5ad24c86c3bbc49fb607ec8b955fa2ad3e98dce
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-17 21:11:32 +0800

    fix: build.py

--- build.py
+++ build.py
@@ -2,5 +2,9 @@ from module.ocr.install_ocr import InstallOcr
 from tasks.weekly.universe import Universe
 import sys
 
+if len(sys.argv) > 1 and sys.argv[1] == "github-actions":
+    from managers.config_manager import config
+    config.set_value("github_mirror", "")
+
 if not InstallOcr.run() or not Universe.update():
     sys.exit(1)
