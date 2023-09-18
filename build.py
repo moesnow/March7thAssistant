cb6b3fdaeb34a6ac1975ef82e019b91de0c4268e
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-18 21:03:46 +0800

    refactor: update build.py

--- build.py
+++ build.py
@@ -2,9 +2,10 @@ from module.ocr.install_ocr import InstallOcr
 from tasks.weekly.universe import Universe
 import sys
 
-if len(sys.argv) > 1 and sys.argv[1] == "github-actions":
-    from managers.config_manager import config
-    config.set_value("github_mirror", "")
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "github-actions":
+        from managers.config_manager import config
+        config.set_value("github_mirror", "")
 
-if not InstallOcr.run() or not Universe.update():
-    sys.exit(1)
+    if not InstallOcr.run() or not Universe.update():
+        sys.exit(1)
