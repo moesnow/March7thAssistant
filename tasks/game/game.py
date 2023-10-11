41b7d39074c19fc134623f1e1abbb01fecc5438f
moesnow <11678347+moesnow@users.noreply.github.com>
2023-10-11 11:56:55 +0800

    feat: auto detect wallpaper

--- tasks/game/game.py
+++ tasks/game/game.py
@@ -1,4 +1,5 @@
 from managers.logger_manager import logger
+from managers.screen_manager import screen
 from managers.automation_manager import auto
 from managers.translate_manager import _
 from managers.config_manager import config
@@ -18,6 +19,16 @@ class Game:
             logger.error(_("⚠️启动游戏超时，退出程序⚠️"))
             input(_("按回车键关闭窗口. . ."))
             sys.exit(1)
+        # 判断手机壁纸
+        if not screen.check_screen('menu'):
+            screen.change_to('menu')
+        if not auto.find_element("./assets/images/menu/journey.png", "image", 0.9):
+            logger.info(_("检测到未使用无名路途壁纸"))
+            screen.change_to('wallpaper')
+            auto.click_element("./assets/images/menu/wallpaper/journey.png", "image", 0.9)
+            auto.click_element("更换", "text",max_retries=10)
+            auto.press_key("esc")
+            logger.info(_("更换到无名路途壁纸成功"))
         logger.hr(_("完成"), 2)
 
     @staticmethod
