302f8d452e7cd272698c95d648fdde6e7e46edb9
moesnow <11678347+moesnow@users.noreply.github.com>
2023-10-20 10:26:10 +0800

    fix: reward

--- tasks/reward/reward.py
+++ tasks/reward/reward.py
@@ -21,15 +21,17 @@ class Reward:
             "mail": lambda: auto.find_element("./assets/images/menu/mail_reward.png", "image", 0.9, take_screenshot=False, crop=(0.95, 0.1, 0.05, 0.6)),
             "assist": lambda: auto.find_element("./assets/images/menu/assist_reward.png", "image", 0.9, take_screenshot=False),
             "dispatch": lambda: auto.find_element("./assets/images/menu/dispatch_reward.png", "image", 0.95, take_screenshot=False),
-            "quest": lambda: auto.find_element("./assets/images/menu/quest_reward.png", "image", 0.95, take_screenshot=False),
-            "srpass": lambda: auto.find_element("./assets/images/menu/pass_reward.png", "image", 0.95, take_screenshot=False),
+            # "quest": lambda: auto.find_element("./assets/images/menu/quest_reward.png", "image", 0.95, take_screenshot=False),
+            # "srpass": lambda: auto.find_element("./assets/images/menu/pass_reward.png", "image", 0.95, take_screenshot=False),
         }
 
         for reward_name, reward_function in reward_mapping.items():
             if reward_function():
                 reward_list.append(reward_name)
 
+        flag = False
         if len(reward_list) != 0:
+            flag = True
             if "mail" in reward_list:
                 logger.hr(_("检测到邮件奖励"), 2)
                 Mail.get_reward()
@@ -42,15 +44,20 @@ class Reward:
                 logger.hr(_("检测到委托奖励"), 2)
                 Dispatch.get_reward()
                 logger.info(_("委托奖励完成"))
-            if "quest" in reward_list:
-                logger.hr(_("检测到每日实训奖励"), 2)
-                Quest.get_reward()
-                logger.info(_("领取每日实训奖励完成"))
-            if "srpass" in reward_list:
-                logger.hr(_("检测到无名勋礼奖励"), 2)
-                SRPass.get_reward()
-                logger.info(_("领取无名勋礼奖励完成"))
-        else:
+
+        # 每日实训和无名勋礼需要实时检测
+        if auto.find_element("./assets/images/menu/quest_reward.png", "image", 0.95):
+            flag = True
+            logger.hr(_("检测到每日实训奖励"), 2)
+            Quest.get_reward()
+            logger.info(_("领取每日实训奖励完成"))
+        if auto.find_element("./assets/images/menu/pass_reward.png", "image", 0.95):
+            flag = True
+            logger.hr(_("检测到无名勋礼奖励"), 2)
+            SRPass.get_reward()
+            logger.info(_("领取无名勋礼奖励完成"))
+
+        if not flag:
             logger.info(_("未检测到任何奖励"))
 
         logger.hr(_("完成"), 2)
