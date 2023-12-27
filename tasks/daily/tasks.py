9a38b360d815552a17196981429e7073aae65406
moesnow <11678347+moesnow@users.noreply.github.com>
2023-12-27 14:19:13 +0800

    fix: daily_tasks

--- tasks/daily/tasks.py
+++ tasks/daily/tasks.py
@@ -39,5 +39,5 @@ class Tasks:
 
     def scroll(self):
         auto.click_element("./assets/images/quest/activity.png", "image", 0.95, crop=self.crop)
-        auto.mouse_scroll(18, -1)
-        time.sleep(0.5)
+        auto.mouse_scroll(36, -1)
+        time.sleep(1)
