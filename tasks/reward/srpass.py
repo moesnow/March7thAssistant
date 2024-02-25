50272cf5a3b52bdc70de3c9470f2d35158716347
moesnow <11678347+moesnow@users.noreply.github.com>
2024-02-25 15:55:34 +0800

    feat: remove useless

--- tasks/reward/srpass.py
+++ tasks/reward/srpass.py
@@ -1,7 +1,6 @@
 from managers.screen_manager import screen
 from managers.automation_manager import auto
 from managers.translate_manager import _
-from tasks.base.base import Base
 from .rewardtemplate import RewardTemplate
 import time
 
@@ -18,7 +17,3 @@ class SRPass(RewardTemplate):
             # 若没解锁则领取奖励
             if auto.click_element("./assets/images/zh_CN/reward/pass/one_key_receive.png", "image", 0.8):
                 auto.click_element("./assets/images/zh_CN/base/click_close.png", "image", 0.8, max_retries=10)
-
-        # 判断是否满级
-        if auto.find_element("./assets/images/share/reward/pass/50.png", "image", 0.9):
-            Base.send_notification_with_screenshot(_("🎉当前版本无名勋礼已满级🎉"))
