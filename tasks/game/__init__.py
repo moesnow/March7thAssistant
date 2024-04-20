1d4d37560fa00881992b6030a26c53119f00d4e5
RehemChan <110209059+RehemChan@users.noreply.github.com>
2024-04-20 15:13:28 +0800

    改回了使用模拟按键输入（使用剪贴板粘贴，账号密码容易被Ditto等软件记录到剪贴板历史）

--- tasks/game/__init__.py
+++ tasks/game/__init__.py
@@ -4,8 +4,6 @@ import time
 import psutil
 import random
 
-import pyautogui
-import pyperclip
 
 from app.tools.account_manager import load_acc_and_pwd
 from utils.registry.gameaccount import gamereg_uid
@@ -194,10 +192,20 @@ def notify_after_finish_not_loop():
 
 def auto_login():
     def auto_type(text):
-        pyperclip.copy(text)
-        time.sleep(0.5)
-        pyautogui.hotkey('ctrl', 'v')
-        pyperclip.copy("")
+        after_alpha = False
+        for character in text:
+            if character.isalpha():
+                after_alpha = True
+            else:
+                if after_alpha:
+                    after_alpha = False
+                    # 切换两下中英文模式，避免中文输入法影响英文输入
+                    auto.secretly_press_key("shift", wait_time=0.1)
+                    auto.secretly_press_key("shift", wait_time=0.1)
+            auto.secretly_press_key(character, wait_time=0.1)
+        auto.secretly_press_key("shift", wait_time=0.1)
+        auto.secretly_press_key("shift", wait_time=0.1)
+        time.sleep(2)
 
     account, password = load_acc_and_pwd(gamereg_uid())
     if auto.click_element("./assets/images/screen/account_and_password.png", "image", 0.9, max_retries=10):
@@ -207,8 +215,6 @@ def auto_login():
                 auto_type(password)
                 if auto.click_element("./assets/images/screen/agree_conditions.png", "image", 0.9, max_retries=10):
                     if auto.click_element("./assets/images/screen/enter_game.png", "image", 0.9, max_retries=10):
-                        if not auto.find_element("./assets/images/screen/welcome.png", "image", 0.9, max_retries=10):
-                            raise Exception("尝试自动登录失败")
-                        else:
+                        if auto.find_element("./assets/images/screen/welcome.png", "image", 0.9, max_retries=10):
                             return
     raise Exception("尝试自动登录失败")
