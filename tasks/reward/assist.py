c089db2ad95b0821eae68b063d919b8e01ae39b0
moesnow <11678347+moesnow@users.noreply.github.com>
2024-05-09 13:53:26 +0800

    Fix: assist

--- tasks/reward/assist.py
+++ tasks/reward/assist.py
@@ -1,8 +1,9 @@
 from module.automation import auto
 from .rewardtemplate import RewardTemplate
-
+import time
 
 class Assist(RewardTemplate):
     def run(self):
         if auto.click_element("./assets/images/share/reward/assist/gift.png", "image", 0.9):
-            auto.click_element("./assets/images/zh_CN/base/click_close.png", "image", 0.8, max_retries=10)
+            time.sleep(1)
+            auto.press_key("esc")
