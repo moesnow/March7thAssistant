46099be0600adbfeb42f94bc0085be746a5c5143
Bella kira <2374035698@qq.com>
2024-12-09 22:37:42 +0800

    fix bug apocalyptic

--- tasks/challenge/apocalyptic.py
+++ tasks/challenge/apocalyptic.py
@@ -82,8 +82,11 @@ class Apocalyptic(BaseChallenge):
             if not self.start_challenge(level):
                 log.error(f"第{level}层挑战失败")
                 break
-
+            time.sleep(1)
+            if auto.find_element("3星通关所有", 'text', include=True):
+                auto.click_element("确认", 'text', max_retries=10, include=True)
             time.sleep(2)
+            
             screen.wait_for_screen_change('apocalyptic')
 
     def find_level(self, level, max_retries=4):
