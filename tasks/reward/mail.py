39f1582e2352829de06ae7d37275af0b7eaf5bff
moesnow <11678347+moesnow@users.noreply.github.com>
2025-12-08 13:40:20 +0800

    fix: 添加邮件奖励领取通知功能

--- tasks/reward/mail.py
+++ tasks/reward/mail.py
@@ -1,8 +1,13 @@
 from module.automation import auto
 from .rewardtemplate import RewardTemplate
+import time
+from module.notification import notif
+from module.notification.notification import NotificationLevel
 
 
 class Mail(RewardTemplate):
     def run(self):
         if auto.click_element("./assets/images/zh_CN/reward/mail/receive_all.png", "image", 0.8):
+            time.sleep(2)
+            notif.notify("邮件奖励已领取", level=NotificationLevel.ALL)
             auto.click_element("./assets/images/zh_CN/base/click_close.png", "image", 0.8, max_retries=10)
