9d448755c3d258b028a2b60e5ba8285ea9dadb6d
moesnow <11678347+moesnow@users.noreply.github.com>
2025-12-12 12:01:42 +0800

    fix: 修改邮件奖励通知的发送方式

--- tasks/reward/mail.py
+++ tasks/reward/mail.py
@@ -1,7 +1,7 @@
 from module.automation import auto
 from .rewardtemplate import RewardTemplate
 import time
-from module.notification import notif
+from tasks.base.base import Base
 from module.notification.notification import NotificationLevel
 
 
@@ -9,5 +9,5 @@ class Mail(RewardTemplate):
     def run(self):
         if auto.click_element("./assets/images/zh_CN/reward/mail/receive_all.png", "image", 0.8):
             time.sleep(2)
-            notif.notify("邮件奖励已领取", level=NotificationLevel.ALL)
+            Base.send_notification_with_screenshot("邮件奖励已领取", level=NotificationLevel.ALL)
             auto.click_element("./assets/images/zh_CN/base/click_close.png", "image", 0.8, max_retries=10)
